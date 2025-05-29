1b2edd5328260eae76920bdcf9edd3825569eac3
Ana Julia Molinos <anamolinos52@gmail.com>
2025-05-29 10:54:15 -0300

    Initial Commit

---
+++ cine_file/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cine_file
+//
+//  Created by Ana Julia on 29/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
