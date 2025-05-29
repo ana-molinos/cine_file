99ab047b64245b832cf3e52a0dcbd8dc967d0049
Ana Julia Molinos <anamolinos52@gmail.com>
2025-05-29 10:54:15 -0300

    Initial Commit

---
+++ cine_file/cine_fileApp.swift
@@ -0,0 +1,17 @@
+//
+//  cine_fileApp.swift
+//  cine_file
+//
+//  Created by Ana Julia on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct cine_fileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
