e6aedd8a09cf6d23f7ceff157011c913ad2b7f5a
Ana Julia Molinos <anamolinos52@gmail.com>
2025-05-29 11:30:39 -0300

    commit incial

--- cine_file/ContentView.swift
+++ cine_file/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack()
+        {
+            Rectangle()
+                .fill(Color.AZUL)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+            //Image()
+            
         }
-        .padding()
     }
 }
 
