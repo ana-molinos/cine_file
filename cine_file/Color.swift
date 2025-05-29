d089b65d4ef9eaf69fd546005ac4938ebdda4dfd
Ana Julia Molinos <anamolinos52@gmail.com>
2025-05-29 11:30:39 -0300

    commit incial

---
+++ cine_file/Color.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+extension Color {
+    static let AZUL = Color(
+        red:   27   / 255,
+        green: 73   / 255,
+        blue:  255  / 255
+    )
+
+    static let VERDE = Color(
+        red:   29   / 255,
+        green: 241  / 255,
+        blue:  135  / 255
+    )
+    
+    static let ESCURO = Color(
+        red:   9   / 255,
+        green: 15  / 255,
+        blue:  32  / 255
+    )
+    
+    static let ROSA = Color(
+        red:   255  / 255,
+        green: 92   / 255,
+        blue:  146  / 255
+    )
+}
