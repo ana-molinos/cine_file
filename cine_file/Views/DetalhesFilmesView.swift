81a1e47737dd9ce422f4844f353eaf678a72180f
Gustavinho1313 <gh.gustavohansen@gmail.com>
2025-05-30 11:34:06 -0300

    Detalhes Filmes view
    oii anaaaa bjsss

---
+++ cine_file/Views/DetalhesFilmesView.swift
@@ -0,0 +1,17 @@
+//
+//  DetalhesFilmesView.swift
+//  cine_file
+//
+//  Created by Gustavo Hansen on 30/05/25.
+//
+import SwiftUI
+
+struct DetalhesFilmesView: View {
+    var body: some View {
+        Text("DetalhesFilmesView")
+    }
+}
+#Preview {
+    DetalhesFilmesView()
+    
+}
