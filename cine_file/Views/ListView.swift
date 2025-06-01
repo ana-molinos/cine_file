aba194e0bcd0929e7a77eb4675348fe868ffce5a
ana-molinos <anamolinos52@gmail.com>
2025-06-01 20:46:46 -0300

    finalição da view de listas

--- cine_file/Views/ListView.swift
+++ cine_file/Views/ListView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ListView: View{
     
-    /// "Uma constante chamada films do tipo lista de objetos do tipo FilmModel recebe o retorno da função films() implementada na extension [FilmModel]"
-    let films: [FilmModel] = [FilmModel].films()
+    /// "Uma propriedade constante chamada films, do tipo lista de objetos do tipo FilmModel, recebe o retorno da função films(), implementada na extension [FilmModel]"
+    private let films: [FilmModel] = [FilmModel].films()
+    
+    // Booleanos para lógica de favoritos e assistidos
+    @State private var viewFavs : Bool = false
+    @State private var viewWatch : Bool = false
+    
+    /// @State marca a propriedade como mutável para que o body possa alterá-la
+    /// Toda vez que essa propriedade é alterada o wrapper @State permite que a view seja renderizada novamente para atualizar (ou seja, chama o body novamente)
+    /// OBS.: não esquecer que deve-se interpretar essa parte como propriedades e não apenas variáveis!!!!!!!!!!
     
     var body: some View{
             ZStack{
@@ -20,42 +28,117 @@ struct ListView: View{
                 
                 ScrollView{
                     
-                    // MARK: Stack da lista
-                    VStack(alignment: .leading, spacing: 30){
+                    // MARK: Stack principal dos elementos na tela
+                    VStack(spacing: 55){
                         
-                        ForEach(films){ film in
+                        // MARK: Stack horizontal para os botões do cabeçalho
+                        HStack(spacing: 30){
                             
-                            //MARK: Stack do card
-                            HStack(spacing: 15){
+                            // Botão de retorno
+                            Button {
+                                // TODO: Codar retorno para a homepage
+                            } label: {
                                 
-                                // Capa do filme
-                                Image(film.imgName)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 100, height: 100)
-                                    .clipped()
-                                    .cornerRadius(8)
+                                // Símbolo de voltar para a home
+                                Image(systemName: "chevron.backward")
+                                    .foregroundStyle(Color.VERDE)
+                                    .font(.system(size: 30))
+                            }
+                            
+                            Spacer()
+                            
+                            // Botão de assistidos
+                            Button {
                                 
-                                // MARK: Stack do texto no card
-                                VStack(alignment: .leading, spacing: 6){
+                                viewWatch.toggle()
+                                /// O .toggle() é uma forma abreviada de inverter o valor do booleano (ou seja, é um método self = !self para mudar o valor p/ true ou false conforme o valor atual)
+                                
+                            } label: {
+                                
+                                // Símbolo de coração
+                                Image(systemName: viewWatch ?  "checkmark.circle.fill" : "checkmark.circle")
+                                    .foregroundStyle(Color.VERDE)
+                                    .font(.system(size: 30))
+                            }
+
+                            // Botão de favoritos
+                            Button {
+                                
+                                viewFavs.toggle()
+                                /// O .toggle() é uma forma abreviada de inverter o valor do booleano (ou seja, é um método self = !self para mudar o valor p/ true ou false conforme o valor atual)
+                                
+                            } label: {
+                                
+                                // Símbolo de coração
+                                Image(systemName: viewFavs ?  "heart.fill" : "heart")
+                                    .foregroundStyle(Color.VERDE)
+                                    .font(.system(size: 30))
+                            }
+                            
+                        }
+                        .padding(.horizontal, 30)
+                        .padding(.top, 40)
+                        
+                        // Título da lista
+                        
+                        
+                        // MARK: Stack da lista
+                        VStack(alignment: .leading, spacing: 30){
+                            
+                            // Laço para mostrar os filmes em cards conforme a condicional
+                            ForEach(films){ film in
+                                
+                                /// Como funciona essa condicional:
+                                /// Se viewFavs é falso ela vai ser verdadeira em todas as iterações e vai mostrar todos os filmes
+                                ///  Se for true, vai reprovar na primeira condição e testar a segunda: se o filme for favorito, ele vai ser exibido na lista
+                                if (viewFavs == false && viewWatch == false) /// Botões não selecionados: lista todos os filmes
+                                || (viewFavs && !viewWatch && film.favorited) /// Botão Fav selecionado: lista os filmes favoritados
+                                || (viewWatch && !viewFavs && film.watched) /// Botão Check selecionado: lista os filmes assistidos
+                                || (film.favorited && film.watched) /// Ambos os botões selecionados: lista os filmes favoritados e que já foram assistidos
+                                {
                                     
-                                    // Título
-                                    Text(film.title)
-                                        .foregroundStyle(Color.white)
-                                        .font(.system(size: 20))
-                                        .bold()
-                                    
-                                    // Direção
-                                    Text(film.direction)
-                                        .foregroundStyle(Color.white)
-                                    
-                                    // Ano
-                                    Text(film.year)
-                                        .foregroundStyle(Color.white)
+                                    //MARK: Stack do card
+                                    HStack(/*alignment: .top,*/ spacing: 15){
+                                        
+                                        // Capa do filme
+                                        Image(film.imgName)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 100, height: 1.34*100)
+                                            .clipped()
+                                            .cornerRadius(8)
+                                        
+                                        // MARK: Stack do texto no card
+                                        VStack(alignment: .leading, spacing: 8){
+                                            
+                                            // Título
+                                            Text(film.title)
+                                                .foregroundStyle(Color.white)
+                                                .font(.system(size: 18))
+                                                .fontWeight(.heavy)
+                                            
+                                            // Direção
+                                            Text(film.direction)
+                                                .foregroundStyle(Color.white)
+                                            
+                                            // Ano
+                                            Text(film.year)
+                                                .foregroundStyle(Color.white)
+                                        }
+                                    }
+                                    /// O frame mantém o card ocupando toda a tela horizontalmente e configura os elementos do card
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(15)
+                                    .background(Color.ESCURO)
+                                    .cornerRadius(15)
+                                    .overlay(
+                                            RoundedRectangle(cornerRadius: 15)
+                                                .strokeBorder(Color.gray, lineWidth: 0.5) // cor e espessura da borda
+                                        )
                                 }
                             }
                         }
-                        
+                        .padding(.horizontal, 30)
                     }
                 }
             }
